7ee848736d3a76faf173bb235a50dc8ad2b3683f
zhang <18577787964@sina.cn>
2022-03-11 14:08:30 +0800

    masteer commit second

--- src/main/java/com/example/HelloGit/Hello.java
+++ src/main/java/com/example/HelloGit/Hello.java
@@ -7,6 +7,6 @@ package com.example.HelloGit;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("master first");
+        System.out.println("master second");
     }
 }
