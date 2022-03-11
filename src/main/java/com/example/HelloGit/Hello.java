d400ece8017892ad4a734a39635b2f3536686d1b
zhang <18577787964@sina.cn>
2022-03-11 13:45:13 +0800

    first commit

---
+++ src/main/java/com/example/HelloGit/Hello.java
@@ -0,0 +1,11 @@
+package com.example.HelloGit;
+
+/**
+ * author ye
+ * createDate 2022/3/11  13:39
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
