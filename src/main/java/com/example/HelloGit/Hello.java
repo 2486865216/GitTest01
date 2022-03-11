7970fa593e4b187b72ac2a2fb7e049ad7106c268
zhang <18577787964@sina.cn>
2022-03-11 13:46:32 +0800

    second commit

--- src/main/java/com/example/HelloGit/Hello.java
+++ src/main/java/com/example/HelloGit/Hello.java
@@ -6,6 +6,6 @@ package com.example.HelloGit;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello git");
     }
 }
